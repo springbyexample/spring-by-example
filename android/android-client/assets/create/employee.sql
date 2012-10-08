279908f869e9bbcba9af325c7cc35342ce3b03e5
Anatolik <a.kaverin@gmail.com>
2012-10-08 08:59:01 +0300

    added empty default schema

---
+++ android/android-client/assets/create/employee.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "employee" ("_id" INTEGER PRIMARY KEY ,"id" INTEGER,"first_name" TEXT  NOT NULL ,"second_name" TEXT  NOT NULL ,"project" TEXT  NOT NULL,"sync_status" INTEGER DEFAULT ('0') )
